2867e759a9a9aa743b836b9dc5176937a4b428ff
JayanthJakkula <jayanthyadavjakkula@gmail.com>
2025-09-22 14:35:03 +0530

    Add files via upload

---
+++ supabase/add_members.py
@@ -0,0 +1,22 @@
+# add_product.py
+import os
+from supabase import create_client, Client #pip install supabase
+from dotenv import load_dotenv # pip install python-dotenv
+ 
+load_dotenv()
+ 
+url = os.getenv("SUPABASE_URL")
+key = os.getenv("SUPABASE_KEY")
+sb: Client = create_client(url, key)
+ 
+def add_product(name, email):
+    payload = {"name": name, "email":email}
+    resp = sb.table("members").insert(payload).execute()
+    return resp.data
+ 
+if __name__ == "__main__":
+    name = input("Enter member name: ").strip()
+    email= input("Enter email: ").strip()
+ 
+    created = add_product(name,email)
+    print("Inserted:", created)
